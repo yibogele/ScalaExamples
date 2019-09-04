30bf59731dba94b71fc474913b3124412a036ed8
yibogele <596510752@qq.com>
2019-09-04 14:28:38 +0800

    add

---
+++ src/main/scala/com/fcx/s4i/ch01/sample01.scala
@@ -0,0 +1,11 @@
+package com.fcx.s4i.ch01
+
+/**
+  * Author: Will Fan
+  * Created: 2019/9/4 11:38
+  * Description: 
+  */
+object sample01 extends App {
+
+  println("Hello".intersect("World"))
+}
