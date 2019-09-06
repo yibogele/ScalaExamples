61594e3b56a7188f75ff3df3e8a7ce8e0f3efb17
yibogele <596510752@qq.com>
2019-09-06 21:38:59 +0800

    aasf

--- src/main/scala/com/fcx/pis3/ch07/CtrlStruct.scala
+++ src/main/scala/com/fcx/pis3/ch07/CtrlStruct.scala
@@ -3,16 +3,16 @@ package com.fcx.pis3.ch07
 import java.io.File
 
 /**
-  * Author: Will Fan
-  * Description: TODO
-  * Created: 2019/7/7 21:33
-  */
+ * Author: Will Fan
+ * Description: TODO
+ * Created: 2019/7/7 21:33
+ */
 object CtrlStruct extends App {
   val filesHere = new File(".").listFiles
   for (file <- filesHere
        if file.isFile
        if file.getName.endsWith("md")
-  ) println(file)
+       ) println(file)
 
   def fileLines(file: java.io.File) =
     try {
@@ -21,12 +21,12 @@ object CtrlStruct extends App {
 
     }
 
-//  for (
-//    file <- filesHere
-//    if file.getName.endsWith("xml");
-//    line <- fileLines(file)
-//    if line.trim.matches(".*2018.*")
-//  ) println(line)
+  //  for (
+  //    file <- filesHere
+  //    if file.getName.endsWith("xml");
+  //    line <- fileLines(file)
+  //    if line.trim.matches(".*2018.*")
+  //  ) println(line)
 
   for {
     file <- filesHere
