4638f2d57c05499fab50a295e6a56a3ae6ff412d
yibogele <596510752@qq.com>
2020-08-02 17:55:08 +0800

    add new file

---
+++ src/main/scala/com/fcx/s4i/ch01/Ex01.scala
@@ -0,0 +1,10 @@
+package com.fcx.s4i.ch01
+
+/**
+ * Author: Will Fan
+ * Description: TODO
+ * Created: 2020/8/2 15:09
+ */
+object Ex01 {
+
+}
---
+++ src/main/scala/com/fcx/s4i/ch02/Ex01.scala
@@ -0,0 +1,10 @@
+package com.fcx.s4i.ch02
+
+/**
+ * Author: Will Fan
+ * Description: TODO
+ * Created: 2020/8/2 17:50
+ */
+object Ex01 {
+
+}
